f835cbdf0db68e1b7e5e779770ecf9a4bb8a46ad
DadiaZ24 <diogo-uds@hotmail.com>
2024-05-18 21:31:31 +0100

    fix

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: ddias-fe <ddias-fe@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/14 16:01:29 by ddias-fe          #+#    #+#             */
-/*   Updated: 2024/05/18 21:23:02 by ddias-fe         ###   ########.fr       */
+/*   Updated: 2024/05/18 21:30:54 by ddias-fe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,14 +20,14 @@ int main(void)
     int     i = 1000;
     int     j = -100;
     
-    img.path = "img/fog.xpm";
+    img.path = "img/fog2.xpm";
     mlx = mlx_init();
     img.img = mlx_xpm_file_to_image(mlx, img.path, &img.width, &img.height);
     mlx_window = mlx_new_window(mlx, 1920, 1080, "LINDOOOOOOO!!!!");
     mlx_put_image_to_window(mlx, mlx_window, img.img, j, 100);
     while (i--)
     {
-        j += 100;
+        j += 10;
         mlx_clear_window(mlx, mlx_window);
         mlx_put_image_to_window(mlx, mlx_window, img.img, j, 100);
         usleep(10000);
