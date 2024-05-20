7a7d8a644e0dee33d54a1ce879228feb26d8deb5
Diogo Dias Ferreira <ddias-fe@c1r9s5.42porto.com>
2024-05-20 21:16:19 +0100

    update

--- include/so_long.h
+++ include/so_long.h
@@ -6,7 +6,7 @@
 /*   By: ddias-fe <ddias-fe@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/14 16:02:57 by ddias-fe          #+#    #+#             */
-/*   Updated: 2024/05/15 19:17:02 by ddias-fe         ###   ########.fr       */
+/*   Updated: 2024/05/20 21:00:25 by ddias-fe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,8 @@
 
 # include "../libs/minilibx_linux/mlx.h"
 # include "../libs/libft/libft.h"
+# include <X11/keysym.h>
+# include <X11/X.h>
 
 typedef struct  s_data
 {
@@ -23,6 +25,27 @@ typedef struct  s_data
 	int		width;
 	int		height;
 }	t_data;
+
+typedef struct g_event
+{
+	void	*mlx;
+	void	*window;
+	int		x;
+	int		y;
+	t_data	fogu;
+	t_data	fogd;
+	t_data	fogr;
+	t_data	fogl;
+	t_data	blast;
+	int		display;
+	int		blast_active;
+	int		blast_pos;
+}	t_event;
+
+int closewindow(int key, t_event *closing);
+int move_down(int key, t_event *event);
+int move_up(int key, t_event *event);
+int animatedblast(t_event *event);
 
 //void    my_mlx_pixel_put(t_data *data, int x, int y, int color);
 
--- src/main.c
+++ src/main.c
@@ -6,61 +6,155 @@
 /*   By: ddias-fe <ddias-fe@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/14 16:01:29 by ddias-fe          #+#    #+#             */
-/*   Updated: 2024/05/18 22:09:39 by ddias-fe         ###   ########.fr       */
+/*   Updated: 2024/05/20 21:13:24 by ddias-fe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/so_long.h"
 
+int closewindow(int key, t_event *event)
+{
+    if (key == XK_Escape)
+    {
+        mlx_clear_window(event->mlx, event->window);
+        mlx_destroy_window(event->mlx, event->window);
+        exit(0);
+    }
+    return (0);
+}
+
+int move_down(int key, t_event *event)
+{
+    if (key == XK_Down)
+    {
+        event->display = 1;
+        event->y += 10;
+        mlx_clear_window(event->mlx, event->window);
+        mlx_put_image_to_window(event->mlx, event->window, event->fogd.img, event->x, event->y);
+        usleep(1000);
+    }
+    return (0);
+}
+
+int move_up(int key, t_event *event)
+{
+    if (key == XK_Up)
+    {
+        event->display = 2;
+        event->y -= 10;
+        mlx_clear_window(event->mlx, event->window);
+        mlx_put_image_to_window(event->mlx, event->window, event->fogu.img, event->x, event->y);
+        usleep(1000);
+    }
+    return (0);
+}
+
+int move_right(int key, t_event *event)
+{
+    if (key == XK_Right)
+    {
+        event->display = 3;
+        event->x += 10;
+        mlx_clear_window(event->mlx, event->window);
+        mlx_put_image_to_window(event->mlx, event->window, event->fogr.img, event->x, event->y);
+        usleep(1000);
+    }
+    return (0);
+}
+
+int move_left(int key, t_event *event)
+{
+    if (key == XK_Left)
+    {
+        event->display = 4;
+        event->x -= 10;
+        mlx_clear_window(event->mlx, event->window);
+        mlx_put_image_to_window(event->mlx, event->window, event->fogl.img, event->x, event->y);
+        usleep(1000);
+    }
+    return (0);
+}
+
+int blastshot(int key, t_event *event)
+{
+    if (key == XK_space && event->display == 3)
+    {
+        event->blast_active = 1;
+        event->blast_pos = 0;
+    }
+    return (0);
+}
+
+int animate_blast(t_event *event)
+{
+    if (event->blast_active)
+    {
+        if (event->blast_pos < 350)
+        {
+            mlx_clear_window(event->mlx, event->window);
+            if (event->display == 3)
+            {
+                mlx_put_image_to_window(event->mlx, event->window, event->fogr.img, event->x, event->y);
+                mlx_put_image_to_window(event->mlx, event->window, event->blast.img, event->x + event->blast_pos + 100, event->y + 45);
+                event->blast_pos += 10;
+                usleep(10000);
+            }
+        }
+        else
+        {
+            event->blast_active = 0;
+            mlx_clear_window(event->mlx, event->window);
+            mlx_put_image_to_window(event->mlx, event->window, event->fogr.img, event->x, event->y);
+        }
+    }
+    return (0);
+}
+
+
+int key_handler(int key, t_event *event)
+{
+    closewindow(key, event);
+    move_down(key, event);
+    move_up(key, event);
+    move_right(key, event);
+    move_left(key, event);
+    blastshot(key, event);
+    return (0);
+}
+
 int main(void)
 {
-    void    *mlx;
-    void    *mlx_window;
-    t_data  img;
-    t_data  img2;
-    t_data  barney;
-    int     i = 150;
-    int     j = -375;
-    int     k = 3;
-    int     baixo = 30;
-    int     bney = 44;
+    t_event event;
+    //t_data  img;
+/*     t_data  img2;
+    t_data  barney; */
     
-    img.path = "img/fog.xpm";
-    img2.path = "img/fog2.xpm";
-    barney.path = "img/barney.xpm";
-    mlx = mlx_init();
-    img.img = mlx_xpm_file_to_image(mlx, img.path, &img.width, &img.height);
-    img2.img = mlx_xpm_file_to_image(mlx, img2.path, &img2.width, &img2.height);
-    barney.img = mlx_xpm_file_to_image(mlx, barney.path, &barney.width, &barney.height);
-    mlx_window = mlx_new_window(mlx, 1000, 500, "LINDOOOOOOO!!!!");
-    mlx_put_image_to_window(mlx, mlx_window, img.img, j, baixo);
-    while (k--)
-    {
-        while (i--)
-        {
-            j += 10;
-            //mlx_clear_window(mlx, mlx_window);
-            mlx_put_image_to_window(mlx, mlx_window, img.img, j, baixo);
-            usleep(10000);
-        }
-        baixo += 70;
-        while (i++ < 150)
-        {
-            j -= 10;
-            //mlx_clear_window(mlx, mlx_window);
-            mlx_put_image_to_window(mlx, mlx_window, img2.img, j, baixo);
-            usleep(10000);
-        }
-        baixo += 70;
-    }
-    //mlx_clear_window(mlx, mlx_window);
-    mlx_put_image_to_window(mlx, mlx_window, barney.img, 0, baixo);
-    while (bney--)
-    {
-        baixo -= 10;
-        mlx_put_image_to_window(mlx, mlx_window, barney.img, 0, baixo);
-        usleep(10000);
-    }
-    mlx_loop(mlx);
+    //int     i = 150;
+    //int     k = 3;
+    //int     bney = 44;
+    
+    event.x = 100;
+    event.y = 30;
+
+    event.fogr.path = "img/fogright.xpm";
+    event.fogu.path = "img/fogup.xpm";
+    event.fogd.path = "img/fogdown.xpm";
+    event.fogl.path = "img/fogleft.xpm";
+    event.blast.path = "img/blast.xpm";
+/*     img2.path = "img/fog2.xpm";
+    barney.path = "img/barney.xpm"; */
+    event.mlx = mlx_init();
+    event.fogr.img = mlx_xpm_file_to_image(event.mlx, event.fogr.path, &event.fogr.width, &event.fogr.height);
+    event.fogu.img = mlx_xpm_file_to_image(event.mlx, event.fogu.path, &event.fogu.width, &event.fogu.height);
+    event.fogd.img = mlx_xpm_file_to_image(event.mlx, event.fogd.path, &event.fogd.width, &event.fogd.height);
+    event.fogl.img = mlx_xpm_file_to_image(event.mlx, event.fogl.path, &event.fogl.width, &event.fogl.height);
+    event.blast.img = mlx_xpm_file_to_image(event.mlx, event.blast.path, &event.blast.width, &event.blast.height);
+/*     img2.img = mlx_xpm_file_to_image(event.mlx, img2.path, &img2.width, &img2.height);
+    barney.img = mlx_xpm_file_to_image(event.mlx, barney.path, &barney.width, &barney.height); */
+    event.window = mlx_new_window(event.mlx, 1000, 500, "LINDOOOOOOO!!!!");
+    mlx_put_image_to_window(event.mlx, event.window, event.fogr.img, event.x, event.y);
+    mlx_key_hook(event.window, key_handler, &event);
+    mlx_loop_hook(event.mlx, animate_blast, &event);
+    mlx_loop(event.mlx);
     return (0);
 }
